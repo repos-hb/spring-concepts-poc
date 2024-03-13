3abb86d699bb245ad7edde303790c98bf1836c7b
Hirak Bhattacharjee <bh.hirak@gmail.com>
2024-03-13 19:09:31 +0530

    response status http

--- order-service-test/src/main/java/com/hb/orderservice/controller/ProductRestController.java
+++ order-service-test/src/main/java/com/hb/orderservice/controller/ProductRestController.java
@@ -1,7 +1,7 @@
 package com.hb.orderservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductRestController {
@@ -14,5 +14,12 @@ public class ProductRestController {
     @GetMapping(value="/bing", produces = {"application/json", "application/xml"})
     public Order nego(){
         return new Order(101,"laptop","9999","2","19998");
+    }
+
+    // response http status code
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String save(@RequestBody Order order){
+        return order.getId()+" "+order.getItem();
     }
 }
